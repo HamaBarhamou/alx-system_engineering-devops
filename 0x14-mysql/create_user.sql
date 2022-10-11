ac5c4e193e4250501b3920a09d0074b0871cd079
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-11 20:40:56 +0100

    script creat user

---
+++ 0x14-mysql/create_user.sql
@@ -0,0 +1,6 @@
+-- creates the MySQL server user user_0d_1.
+-- user_0d_1 should have all privileges
+-- cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+
+CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT ALL PRIVILEGES ON *.* TO 'holberton_user'@'localhost';
