479626c6d2c0b8f43759d60dfdb62b92dc5c2fed
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-17 12:58:45 +0100

    mise en forme 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,6 +4,20 @@ if __name__ == '__main__':
     import requests
     import sys
 
-    r = requests.get('https://jsonplaceholder.typicode.com/todos/{}'.format(sys.argv[1]))
-    print(r.json)
-    pass
+    EMPLOYEE_NAME = "name"
+    NUMBER_OF_DONE_TASKS = 0
+    TOTAL_NUMBER_OF_TASKS = 0
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                     .format(sys.argv[1]))
+    reponse = r.json()
+
+    for loop in reponse:
+        TOTAL_NUMBER_OF_TASKS += 1
+        if loop['completed']:
+            NUMBER_OF_DONE_TASKS += 1
+
+    print("Employee {} is done with tasks({}/{})".format(
+         EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for loop in reponse:
+        print("  ", loop['title'])
