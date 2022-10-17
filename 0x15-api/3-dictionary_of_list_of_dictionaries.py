bc70b168cd0ce0f38a157e6772c581d79f4ff84f
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-17 21:31:13 +0100

    3. Dictionary of list of dictionaries correction 2

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,35 +1,27 @@
 #!/usr/bin/python3
-""" Module of Gather data from an API"""
+"""Exports data in the JSON format"""
 
+if __name__ == "__main__":
 
-from urllib import response
-
-from html2text import ListElement
-
-
-if __name__ == '__main__':
     import json
     import requests
     import sys
 
-    r = requests.get('https://jsonplaceholder.typicode.com/users/')
-    LISTE_USR = r.json()
-    dic = {}
-    for usr in LISTE_USR:
-        id = usr.get('id')
-        res = requests.get(
-                'https://jsonplaceholder.typicode.com/users/{}/todos'
-                .format(id))
-        data = res.json()
-        tab = []
-        for loop in data:
-            js = {}
-            js["username"] = usr.get('username')
-            js["task"] = loop.get('title')
-            js["completed"] = loop.get("completed")
-            tab.append(js)
-        dic[id] = tab
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
 
-    filename = 'todo_all_employees.json'
-    with open(filename, 'w') as mon_fichier:
-        json.dump(dic, mon_fichier)
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
