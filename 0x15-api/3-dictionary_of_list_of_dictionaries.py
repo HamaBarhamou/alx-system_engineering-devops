8d435f3c591aef95526c6f45be289095898df129
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-17 21:22:08 +0100

    3. Dictionary of list of dictionaries

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Module of Gather data from an API"""
+
+
+from urllib import response
+
+from html2text import ListElement
+
+
+if __name__ == '__main__':
+    import json
+    import requests
+    import sys
+
+    r = requests.get('https://jsonplaceholder.typicode.com/users/')
+    LISTE_USR = r.json()
+    dic = {}
+    for usr in LISTE_USR:
+        id = usr.get('id')
+        res = requests.get(
+                'https://jsonplaceholder.typicode.com/users/{}/todos'
+                .format(id))
+        data = res.json()
+        tab = []
+        for loop in data:
+            js = {}
+            js["username"] = usr.get('username')
+            js["task"] = loop.get('title')
+            js["completed"] = loop.get("completed")
+            tab.append(js)
+        dic[id] = tab
+
+    print(dic)
+
+    """filename = sys.argv[1] + '.json'
+    data = {sys.argv[1]: tab}
+    with open(filename, 'w') as mon_fichier:
+        json.dump(data, mon_fichier)"""
