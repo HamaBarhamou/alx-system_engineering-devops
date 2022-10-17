37d904b5face1b65dfc7e70c5683fc082e4033aa
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-10-17 13:57:33 +0100

    using get 2

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -17,12 +17,12 @@ if __name__ == '__main__':
 
     for loop in reponse:
         TOTAL_NUMBER_OF_TASKS += 1
-        if loop['completed']:
+        if loop.get('completed'):
             NUMBER_OF_DONE_TASKS += 1
 
     print("Employee {} is done with tasks({}/{})".format(
          EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
 
     for loop in reponse:
-        if loop['completed']:
+        if loop.get('completed'):
             print("  ", loop['title'])
